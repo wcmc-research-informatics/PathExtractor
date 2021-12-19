e4827de53faef2d8bd093031c72710c55762c44d
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-19 11:37:55 +0800

    Delete runReaderUpdate.sh

--- runReaderUpdate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-// java -cp $CLASSPATH concepts.Test $WORKING_DIR/config/db.properties
-java -Xmx1024m -Xms256m -Dlog4j.configuration=file:config/log4j.properties -cp "lib/*:target/*" gov.va.vinci.ef.ReaderUpdate "config/db.properties" "config/readers/BatchDatabaseCollectionReaderConfig.groovy" 
